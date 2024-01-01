4718bbc8d9cdb849db5e673b42b6d56b67caf39a
mike-subtilis <mike@subtilis-software.com>
2024-01-01 14:08:37 -0700

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,8 @@
 import React from "react";
 
-import logo from "../assets/logo.svg";
-
 const Hero = () => (
   <div className="text-center hero my-5">
-    <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
+    <img className="mb-3 app-logo" alt="React logo" width="120" />
     <h1 className="mb-4">React.js Sample Project</h1>
 
     <p className="lead">
